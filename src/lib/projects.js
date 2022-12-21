5adcd94f88b8b6d023359567a1c82005eaa7b42a
leviptz <levipatzwaldt@icloud.com>
2022-12-21 23:13:53 +0100

    improved project description

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -4,7 +4,7 @@ const projects = [
     {
       name: 'Tradecademy',
       description:
-        'Web application for reading financial analytics and learn the ins and outs of trading from industry experts.',
+        'A web application for reading financial analytics and learn the ins and outs of trading from industry experts.',
       link: { href: 'http://tradecademy.de', label: 'tradecademy.de' },
       logo: tradecademy,
     }
