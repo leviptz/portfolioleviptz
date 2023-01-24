6a3d449a41acb4a4935d29abf3b74648b9eadafa
leviptz <levipatzwaldt@icloud.com>
2023-01-24 21:56:38 +0100

    added project

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,3 +1,4 @@
+import coffeecorner from '@/images/logos/coffeecorner.png'
 import tradecademy from '@/images/logos/tradecademy.png'
 
 const projects = [
@@ -7,6 +8,13 @@ const projects = [
         'A web application for reading financial analytics and learn the ins and outs of trading from industry experts.',
       link: { href: 'http://tradecademy.de', label: 'tradecademy.de' },
       logo: tradecademy,
+    },
+    {
+      name: 'Coffee Corner',
+      description:
+        'A website for a cafe in Brandenburg.',
+      link: { href: 'http://www.coffeecorner-brandenburg.de/', label: 'coffeecorner-brandenburg.de' },
+      logo: coffeecorner,
     }
   ]
 
